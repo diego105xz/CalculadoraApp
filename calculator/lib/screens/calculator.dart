65740c0b2de00b8efa4108f2079c7dec73c02be3
Diego Melo <diego-melo@uni9.edu.br>
2021-04-30 11:58:05 -0300

    Versão 1.0.0
    Aula 1

---
+++ calculator/lib/screens/calculator.dart
@@ -0,0 +1,17 @@
+import 'package:calculator/components/display.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Calculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Column(
+        children: <Widget>[
+          Display('123.45'),
+          Text('Keyboard'),
+        ],
+      ),
+    );
+  }
+}
